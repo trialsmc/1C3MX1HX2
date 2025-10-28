2b66b61474557154501b5aaa807f5092bcbe6dbf
trialsmc <suarezenderson84@hotmail.com>
2025-10-28 13:31:20 -0400

    Update loadscreen.lua

--- loadscreen.lua
+++ loadscreen.lua
@@ -9,7 +9,8 @@ ScreenGui.IgnoreGuiInset = true
 
 local Background = Instance.new("Frame")
 Background.Parent = ScreenGui
-Background.Size = UDim2.new(0, 400, 0, 200)
+Background.AnchorPoint = UDim2.new(0.5, 0.5)
+Background.Size = UDim2.new(0, 380, 0, 190)
 Background.Position = UDim2.new(0.5, 0, 0.5, 0)
 Background.BackgroundColor3 = Color3.fromRGB(25, 0, 0)
 
@@ -22,43 +23,41 @@ Title.Parent = Background
 Title.Size = UDim2.new(1, -40, 0, 60)
 Title.Position = UDim2.new(0, 20, 0, 40)
 Title.BackgroundTransparency = 1
-Title.Text = "HEXAGON HUB"
+Title.Text = "HEXAGON"
 Title.TextColor3 = Color3.fromRGB(255,60,60) -- Color
 Title.Font = Enum.Font.GothamBold
-Title.TextScaled = true
 Title.TextSize = 32
 
 local Desc = Instance.new("TextLabel")
 Desc.Parent = Background
 Desc.Size = UDim2.new(1, -40, 0, 80)
-Desc.Position = UDim2.new(0, 20, 0, 105)
+Desc.Position = UDim2.new(0, 20, 0, 60)
 Desc.BackgroundTransparency = 1
 Desc.Text = "DESCRIPTION"
-Desc.TextColor3 = Color3.fromRGB(255,60,60) -- Color
-Desc.Font = Enum.Font.GothamBold
-Desc.TextScaled = true
+Desc.TextColor3 = Color3.fromRGB(255,180,180) -- Color
+Desc.Font = Enum.Font.Gotham
 Desc.TextSize = 16
 
 local ProgressBar = Instance.new("Frame")
-ProgressBar.Size = UDim2.new(1, -40, 0, 6)
-ProgressBar.Position = UDim2.new(0, 20, 1, -30)
-ProgressBar.BackgroundColor3 = Color3.fromRGB(40, 0, 0)
-ProgressBar.BorderSizePixel = 0
 ProgressBar.Parent = Background
+ProgressBar.Position = UDim2.new(0, 20, 1, -30)
+ProgressBar.Size = UDim2.new(1, -40, 0, 6)
+ProgressBar.BackgroundColor3 = Color3.fromRGB(255, 50, 50)
+ProgressBar.BorderSizePixel = 0
 
 local ProgressCorner = Instance.new("UICorner")
 ProgressCorner.CornerRadius = UDim.new(0.5,0)
 ProgressCorner.Parent = ProgressBar
 
 local PercentText = Instance.new("TextLabel")
-PercentText.Size = UDim2.new(1,0,1,0)
-PercentText.Position = UDim2.new(0,0,0,0)
+PercentText.Parent = Background
+PercentText.Size = UDim2.new(1, 0, 1, 0)
+PercentText.Position = UDim2.new(0, 0, 0, 50)
 PercentText.BackgroundTransparency = 1
-PercentText.TextColor3 = Color3.fromRGB(180,180,180)
-PercentText.Font = Enum.Font.GothamBold
-PercentText.TextScaled = true
 PercentText.Text = "0%"
-PercentText.Parent = BarBackground
+PercentText.TextColor3 = Color3.fromRGB(255,180,180)
+PercentText.Font = Enum.Font.Gotham
+PercentText.TextSize = 12
 
 -- Loading Animation
 local totalTime = 7 -- Seconds
@@ -72,4 +71,5 @@ for i = 1, steps do
 end
 -- Delete GUI in the end
 Blur.Destroy()
+
 ScreenGui:Destroy()
